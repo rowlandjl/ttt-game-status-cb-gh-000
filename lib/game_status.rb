d8af1c4a4042f292d5f177198696157f8113761d
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 01:47:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      combo
+      return combo
     else
       false
     end
