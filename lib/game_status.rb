c3ab37fe584c35a2732f108547fa060027dcb71b
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 01:19:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,5 +3,11 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5]  # Middle row
+  [6,7,8] #Bottom row 
+]
+
 def won?
 end
