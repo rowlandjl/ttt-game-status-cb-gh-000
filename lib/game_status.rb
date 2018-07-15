5c1c094af14c0defe4a162c205d673f4807bf853
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 02:10:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,6 +16,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each_with_index do |element, index|
-    if position_taken?(board, element[0]) == true 
+    if position_taken?(board, element[0]) == true
+      if board[element[0]] == board[element[1]] && board[element[1]] == board[element[2]]
+        return WIN_COMBINATIONS[index]
   end
 end
