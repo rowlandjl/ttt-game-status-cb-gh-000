6f902cc5b760555782ec547842f01d6776dc5ba8
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 01:19:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,6 +7,9 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5]  # Middle row
   [6,7,8] #Bottom row 
+  [0,3,6]  # First column 
+  [1,4,7]  #Second column 
+  [2,5,8]  #Third column 
 ]
 
 def won?
