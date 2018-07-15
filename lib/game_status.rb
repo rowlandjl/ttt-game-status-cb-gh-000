c3dfe1764c46d758623d0d90262d25e3ce1325f0
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 01:20:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,7 +9,7 @@ WIN_COMBINATIONS = [
   [6,7,8] #Bottom row
   [0,3,6]  # First column
   [1,4,7]  #Second column
-  [2,5,8]  #Third column 
+  [2,5,8]  #Third column
   [0,4,8]  #diagonal 1
   [2,4,6]  #diagonal 2 
 ]
